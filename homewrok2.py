1d58fd465d53806536bfcbd76ea9650f5f16cb9f
luka-khabelashvili <luka.khabelashvili1@gmail.com>
2025-06-08 21:42:44 +0400

    homewok-2

---
+++ homewrok2.py
@@ -0,0 +1,35 @@
+message = "hello, world"
+print(message)
+
+message = "hello, earth"
+print(message)
+
+my_name = "luka"
+print(my_name.upper())
+print(my_name.lower())
+print(my_name.title())
+
+print('my favoryte food:\n khinkali\n mtsvadi\n pizza')
+
+text = "            i have white spaace"
+print(text.lstrip())
+
+quote = 'One of Python\' strengths is its diverse community.'
+print(quote)
+
+person_name = "2pac"
+print(f"hello {person_name} would you like to learn some Python today?")
+
+print('Albert Einstein once said,"A person who never made a mistake never tried anything new."')
+ 
+famous_person = "Albert Einstein"
+print( f'{famous_person} once said, "A person who never made a mistake never tried anything new."')
+
+print(4 + 4)
+print(10 - 2)
+print(4 * 2)
+print(16 // 2)
+
+# favorite_number = 7
+# message = f"my favorite number is {favorite_number}"
+# print(message)
